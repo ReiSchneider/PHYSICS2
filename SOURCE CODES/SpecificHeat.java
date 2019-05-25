10b893118d8f024cb880bde39f220c1fbc1205dc
ReiSchneider <kylecancio@gmail.com>
2019-05-25 22:38:37 +0800

    Initial commit

---
+++ SOURCE CODES/SpecificHeat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package specificheat;
+
+/**
+ *
+ * @author Kyle Cancio
+ */
+public class SpecificHeat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpecificHeatGUI x = new SpecificHeatGUI();
+        x.callinit();
+    }
+}
